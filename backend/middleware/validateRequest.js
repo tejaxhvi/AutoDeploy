aeaa43085d70c2c643ad79ea5f94c17b5b34c487
Tejashvi Yadav <tajeshviyadav@gmail.com>
2026-07-07 20:08:29 +0530

    Add request validation middleware with Zod schemas

---
+++ backend/middleware/validateRequest.js
@@ -0,0 +1,11 @@
+export const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body);
+    next() // If valid, move to controller
+  } catch (err) {
+    res.status(404).json({
+      message: "Validation Error",
+      errors: err.errors.map(e => e.message)
+    })
+  }
+}
---
+++ backend/types/userSchema.js
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const signupSchema = z.object({
+  username: z.string().min(3, "Username must be at least 3 characters."),
+  email: z.email("Invalid email format."),
+  password: z.string().min(6,"Password must be at least 6 characters.")
+})
+
+export const signinSchema = z.object({
+  email: z.email("Invalid email format"),
+  password: z.string().min(1, "Password is required"),
+});
