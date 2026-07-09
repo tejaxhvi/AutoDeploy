2b1f97e64b698da155e21d205a7d1de4ab906ad2
Tejashvi Yadav <tajeshviyadav@gmail.com>
2026-07-09 23:13:10 +0530

    feat: implement login functionality with backend POST request

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -1,24 +1,55 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
-export function LoginForm({
-  className,
-  ...props
-}) {
+export function LoginForm({ className, ...props }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      console.log(email, password);
+      const response = await fetch("/api/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log("Response from Backend", data);
+      if (!response.ok) {
+        throw new Error(
+          data.message || "Failed to Login. Please check your credentials.",
+        )
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
@@ -31,27 +62,45 @@ export function LoginForm({
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <form>
+              <form onSubmit={handleLogin}>
                 <FieldGroup>
                   <Field>
                     <FieldLabel htmlFor="email">Email</FieldLabel>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
                   </Field>
                   <Field>
                     <div className="flex items-center">
                       <FieldLabel htmlFor="password">Password</FieldLabel>
                       <a
                         href="#"
-                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
                         Forgot your password?
                       </a>
                     </div>
-                    <Input id="password" type="password" required />
+                    <Input
+                      id="password"
+                      type="password"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
                   </Field>
+
+                  {errorMessage && (
+                    <div className="text-sm text-red-500 text-center font-medium">
+                      {errorMessage}
+                    </div>
+                  )}
+
                   <Field>
-                    <Button type="submit">Login</Button>
-                    <Button variant="outline" type="button">
-                      Login with Google
+                    <Button type="submit" disable={isLoading}>
+                      {isLoading ? "Logging in..." : "Login"}
                     </Button>
                     <FieldDescription className="text-center">
                       Don&apos;t have an account? <a href="/signup">Sign up</a>
