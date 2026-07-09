cd0aa3fe9658564b995f72e64ea8b00757649a2b
Tejashvi Yadav <tajeshviyadav@gmail.com>
2026-07-09 23:00:53 +0530

    fix: resolve incorrect import for Home component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-
-import { Routes, Route } from 'react-router'
-import { SignupForm } from './components/signup-form'
-import { LoginForm } from './components/login-form'
-import { Home } from './pages/Home'
+import { Routes, Route } from "react-router";
+import { SignupForm } from "./components/signup-form";
+import { LoginForm } from "./components/login-form";
+import Home from "./pages/Home";
 
 export default function App() {
-
   return (
-    <div className="h-full w-full">
+    <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<SignupForm />} />
-        <Route path='/login' element={<LoginForm />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </div>
-  )
+  );
 }
